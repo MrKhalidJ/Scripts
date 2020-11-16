fb38c3ef1e90ca8591dba10fd9d916a24a7d4d46
Khalid <kmoj86@gmail.com>
2020-11-16 15:00:41 +0300

    updated app_data_backup.sh

--- app_data_backup/app_data_backup.sh
+++ app_data_backup/app_data_backup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# delete old backup files
+# remove old backup files
 rm /home/admin/MyAppBackup/myapp-data.backup.sql
 rm /home/admin/MyAppBackup/myapp-files-backup.tar.gz
 
 
-#Backup database
+# backup database
 sudo mysqldump -u root myapp > /home/admin/MyAppBackup/myapp-data.backup.sql
 
 
-#Backup files
+# backup files
 sudo tar -czvf /home/admin/MyAppBackup/myapp-files-backup.tar.gz /var/www/myapp/.env /var/www/myapp/public/uploads /var/www/myapp/storage/uploads
 
 
